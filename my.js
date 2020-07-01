6eb253d37603eb86cc37eb49c25417e654f04f84
nishtha777 <64225774+nishtha777@users.noreply.github.com>
2020-07-01 13:32:54 +0530

    Adding new files

---
+++ my.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: true,
+  navText: [
+    "<i class='fa fa-angle-left'></i>",
+    "<i class='fa fa-angle-right'></i>"
+  ],
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 2
+    }
+  }
+})
